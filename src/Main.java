fa5d2acdce3a4825f33ca7b75e229a2cb94d1289
Mariana <mari.anasieveg2@gmail.com>
2017-11-23 13:31:23 -0600

    mainAFD

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,51 @@ public class Main extends Application {
 	//implements EventHandler<ActionEvent>
 	Button button;
 	
+	private static void mainAFD(AFD markdown){
+		markdown.createAFD(22, 6);
+		char[] symbols = {'a','#','*','1','.','`'};
+		markdown.setSymbols(symbols);
+		State auxiliar_state;
+		for(int i = 0; i < 9;i++){
+			auxiliar_state = new State(Integer.toString(i), false, false, i);
+			markdown.addState(auxiliar_state);
+		}
+		
+		markdown.states[0].initial_state = true;
+		markdown.states[8].final_state = true;
+		
+		markdown.states[0].addTransition('a', 0);
+		markdown.states[0].addTransition('#', 1);
+		markdown.states[0].setCSSName("No header");
+		
+		markdown.states[1].addTransition('a', 7);
+		markdown.states[1].addTransition('#', 2);
+		markdown.states[1].setCSSName("H1");
+		
+		markdown.states[2].addTransition('a', 7);
+		markdown.states[2].addTransition('#', 3);
+		markdown.states[2].setCSSName("H2");
+		
+		markdown.states[3].addTransition('a', 7);
+		markdown.states[3].addTransition('#', 4);
+		markdown.states[3].setCSSName("H3");
+		
+		markdown.states[4].addTransition('a', 7);
+		markdown.states[4].addTransition('#', 5);
+		markdown.states[4].setCSSName("H4");
+		
+		markdown.states[5].addTransition('a', 7);
+		markdown.states[5].addTransition('#', 6);
+		markdown.states[5].setCSSName("H5");
+		
+		markdown.states[6].addTransition('a', 7);
+		markdown.states[6].addTransition('#', 7);
+		markdown.states[6].setCSSName("H6");
+		
+		markdown.states[7].addTransition('\n', 8);
+		markdown.states[7].addTransition('a', 7);
+	}
+	
 	private static void headerAFD(AFD markdown){
 		markdown.createAFD(9, 3);
 		char[] symbols = {'a','#','\n'};
