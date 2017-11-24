b4c447e45de0fcf2c95c65cabfc11e5af0f9e557
JulianLz96 <jj_lz96@hotmail.com>
2017-11-23 23:44:12 -0600

    Added Shortcuts

--- src/controllers/TextEditorController.java
+++ src/controllers/TextEditorController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
@@ -10,12 +11,30 @@ import utils.Utils;
 public class TextEditorController {
 	@FXML TextArea body;
 	@FXML MenuItem showPreview;
+	@FXML MenuItem saveItem;
+	@FXML MenuItem closeItem;
 
 	@FXML public void onCreate(ActionEvent event) {
 		sendToAutomaton();
 		Utils.createWindow(null, TextEditorController.this, "fxml/Preview.fxml", "Preview", null, null);
 	}
 
+	@FXML public void showMark(ActionEvent event){
+		Utils.createWindow(null, TextEditorController.this, "fxml/Markdown.fxml", "Markdown Use Example", null, null);
+	}
+	@FXML public void save(ActionEvent event){
+		System.out.println("shortcut");
+	}
+
+	@FXML public void close(ActionEvent event){
+		System.out.println("shortcut");
+		Platform.exit();
+		System.exit(0);
+	}
+
+	@FXML public void showAbout(ActionEvent event){
+		Utils.createWindow(null, TextEditorController.this, "fxml/About.fxml", "About", null, null);
+	}
 	private void sendToAutomaton() {
 		String text = "";
 		text = body.getText().replaceAll("\n", System.getProperty("line.separator"));
