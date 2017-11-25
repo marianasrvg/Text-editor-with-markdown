99c42bdfb5028bd57f27cd70b9cc38e46ba1e504
JulianLz96 <jj_lz96@hotmail.com>
2017-11-24 21:27:21 -0600

    Shortcut Close Preview

--- src/controllers/PreviewController.java
+++ src/controllers/PreviewController.java
@@ -3,16 +3,21 @@ package controllers;
 import java.util.ArrayList;
 
 import interfaces.WindowState;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
 public class PreviewController implements WindowState {
 
 	private ArrayList<Text> content;
 	@FXML private TextFlow textFlow;
 	@FXML private AnchorPane anchorPane;
+	@FXML private MenuItem closeItem;
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -24,6 +29,11 @@ public class PreviewController implements WindowState {
 		for(int i = 0; i < content.size(); i += 3){
 			textFlow.getChildren().addAll(content.get(i) != null ? content.get(i) : new Text(""), content.get(i+1) != null ? content.get(i+1) : new Text(""), content.get(i+2) != null ? content.get(i+2) : new Text(""), new Text("\n"));
 		}
+	}
+
+	@FXML public void close(ActionEvent event){
+		Stage stage = (Stage) anchorPane.getScene().getWindow();
+		stage.close();
 	}
 
 }
